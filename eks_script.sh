98cbc029f95426eb6537983ba91056f5710fc8ff
branyangeks <74394086+branyangeks@users.noreply.github.com>
2020-11-12 23:27:46 -0500

    Add files via upload

---
+++ eks_script.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+#Download v2:
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+
+#Update current AWS CLI Install
+sudo ./aws/install --bin-dir /usr/bin --install-dir /usr/bin/aws-cli --update
+
+#Download kubectl
+sudo curl --silent --location -o /usr/local/bin/kubectl \
+   https://amazon-eks.s3.us-west-2.amazonaws.com/1.17.11/2020-09-18/bin/linux/amd64/kubectl
+
+#Download eksctl
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/bin
+
+
+sudo chmod +x /usr/local/bin/kubectl
+sudo pip install --upgrade awscli && hash -r
+
+#install git
+sudo yum -y install jq gettext bash-completion moreutils
+
+#Install yq for yaml processing
+echo 'yq() {
+  docker run --rm -i -v "${PWD}":/workdir mikefarah/yq yq "$@"
+}' | tee -a ~/.bashrc && source ~/.bashrc
+
+#Verify the binaries are in the path and executable
+for command in kubectl jq envsubst aws
+  do
+    which $command &>/dev/null && echo "$command in path" || echo "$command NOT FOUND"
+  done
+  
+#enable kubectl bash_completion
+kubectl completion bash >>  ~/.bash_completion
+. /etc/profile.d/bash_completion.sh
+. ~/.bash_completion
+
+#set the AWS Load Balancer Controller version
+echo 'export LBC_VERSION="v2.0.0"' >>  ~/.bash_profile
+.  ~/.bash_profile
+
+#We should configure our aws cli with our current region as default.
+export ACCOUNT_ID=$(aws sts get-caller-identity --output text --query Account)
+export AWS_REGION=$(curl -s 169.254.169.254/latest/dynamic/instance-identity/document | jq -r '.region')
+
+#Check if AWS_REGION is set to desired region
+test -n "$AWS_REGION" && echo AWS_REGION is "$AWS_REGION" || echo AWS_REGION is not set
+
+#Validate the IAM role
+aws sts get-caller-identity --query Arn | grep eksworkshop-admin -q && echo "IAM role valid" || echo "IAM role NOT valid"
+
+
