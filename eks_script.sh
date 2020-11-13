4d86f5f8d34e5afa22265b57451281c91f7b1e8f
branyangeks <74394086+branyangeks@users.noreply.github.com>
2020-11-13 01:14:06 -0500

    Update eks_script.sh

--- eks_script.sh
+++ eks_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#Download v2:
+#Download awscli v2:
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 
@@ -19,28 +19,6 @@ sudo mv /tmp/eksctl /usr/bin
 sudo chmod +x /usr/local/bin/kubectl
 sudo pip install --upgrade awscli && hash -r
 
-#install git
-#sudo yum install -y git
-
-#Install yq for yaml processing
-echo 'yq() {
-  docker run --rm -i -v "${PWD}":/workdir mikefarah/yq yq "$@"
-}' | tee -a ~/.bashrc && source ~/.bashrc
-
-#Verify the binaries are in the path and executable
-for command in kubectl jq envsubst aws
-  do
-    which $command &>/dev/null && echo "$command in path" || echo "$command NOT FOUND"
-  done
-  
-#enable kubectl bash_completion
-kubectl completion bash >>  ~/.bash_completion
-. /etc/profile.d/bash_completion.sh
-. ~/.bash_completion
-
-#set the AWS Load Balancer Controller version
-echo 'export LBC_VERSION="v2.0.0"' >>  ~/.bash_profile
-.  ~/.bash_profile
 
 #We should configure our aws cli with our current region as default.
 export ACCOUNT_ID=$(aws sts get-caller-identity --output text --query Account)
@@ -51,5 +29,3 @@ test -n "$AWS_REGION" && echo AWS_REGION is "$AWS_REGION" || echo AWS_REGION is 
 
 #Validate the IAM role
 aws sts get-caller-identity --query Arn | grep eksworkshop-admin -q && echo "IAM role valid" || echo "IAM role NOT valid"
-
-
