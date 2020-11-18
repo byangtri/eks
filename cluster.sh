ea5b5ba6c5314f0d7665c33a9c139bdc54f3df2d
brandon <brandon@cirruslabs.io>
2020-11-18 12:49:43 -0500

    modified:   cluster.sh

--- cluster.sh
+++ cluster.sh
@@ -1,29 +1,7 @@
 #! /usr/sh
 
-# Use the profile which has IAM rights
-export AWS_PROFILE=eks
-echo "Using AWS profile: $AWS_PROFILE"
-
-export AWS_REGION=us-east-1
-echo "Using AWS region: $AWS_REGION"
-
-export K8S_VERSION=1.17
-echo "Using Kubernetes versoin: $K8S_VERSION"
-
-# Check any existing EKS cluster
-eksctl get cluster \
-  --profile $AWS_PROFILE \
-  --region $AWS_REGION
-
-# use profile "eks" and region "us-east-1" 
-# use matching kubectl version e.g. 1.17
-#     Refer: https://docs.aws.amazon.com/eks/latest/userguide/install-kubectl.html
-
-eksctl create cluster --name tricentis-cluster --version $K8S_VERSION --region $AWS_REGION --nodegroup-name standard-workers --node-type t3.micro --nodes 3 --nodes-min 1 --nodes-max 4 --node-ami auto
-
 # After 10 min or so, confirm again for desired EKS cluster
 eksctl get cluster \
-  --profile $AWS_PROFILE \
   --region $AWS_REGION
 
 # Typically the config file would be stored as
