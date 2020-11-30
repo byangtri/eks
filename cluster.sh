498b0d7e5a78080750a1bec93ec5b572e61c5968
brandon <brandon@cirruslabs.io>
2020-11-30 13:21:32 -0500

    update

--- cluster.sh
+++ cluster.sh
@@ -42,7 +42,7 @@ kubectl get pods
 
 ### === MySQL pod === ###
 # Create MySQL pod
-kubectl create -f mysql-deployment.yaml
+kubectl create -f postgres-deployment.yaml
 # Confirm the pod itself
 kubectl get pods
 
